3a0c824e9d6f95a6dcd66e97b04576dc4c87c09f
Samson Ocran <samson.ocran@gmail.com>
2022-11-06 18:51:58 +0100

    updating

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -3,30 +3,30 @@ import axios from "axios";
 import Button from "../components/button/Button";
 import "./users.scss";
 
-const Users = () => {
-  const [userData, setUserData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [activeUser, setActiveUser] = useState(false);
+// const Users = () => {
+//   const [userData, setUserData] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [activeUser, setActiveUser] = useState(false);
 
-  const url = "https://randomuser.me/api/"
+//   const url = "https://randomuser.me/api/"
 
-  const handleClick = () => {
-    setLoading(true);
-    axios
-      .get(url)
-      .then((response) => {
-        console.log(response.data.results);
-        setUserData(response.data.results);
-      })
-      .catch((error) => {
-        console.log(error);
-        setLoading(true);
-      })
-      .finally(() => {
-        setLoading(false);
-        setActiveUser(true);
-      });
-  };
+//   const handleClick = () => {
+//     setLoading(true);
+//     axios
+//       .get(url)
+//       .then((response) => {
+//         console.log(response.data.results);
+//         setUserData(response.data.results);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         setLoading(true);
+//       })
+//       .finally(() => {
+//         setLoading(false);
+//         setActiveUser(true);
+//       });
+//   };
 
   return (
     <div className="users-container">
